d24e6c283f8185f297f9574060a858407fa4f0e0
builtbyedgar <hello@builtbyedgar.com>
2017-06-21 21:30:41 +0200

    isInteger method

---
+++ src/isInteger.js
@@ -0,0 +1,22 @@
+
+/**
+ * Check if the parameter is an Integer or not.
+ *
+ * @param {Number} value Numer to test
+ * @return {Boolean} Return `true` if value is an Integer, `false` otherwise
+ * @example
+ *
+ *  isInteger(1)  // true
+ *  isInteger(123)  // true
+ *  isInteger(1.23)  // false
+ *  isInteger('')  // false
+ *  isInteger('123')  // false
+ *  isInteger(true)  // false
+ *  isInteger(false)  // false
+ *  isInteger(NaN)  // false
+ *  isInteger([])  // false
+ *  isInteger(() => {})  // false
+ *
+ */
+
+export default isInteger = (value) => (value << 0) === value
