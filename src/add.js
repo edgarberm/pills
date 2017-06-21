8ed1b5f03d6129450fc0585c0447a6f58a1d3cf8
builtbyedgar <hello@builtbyedgar.com>
2017-06-21 19:28:15 +0200

    edited comments add method

--- src/add.js
+++ src/add.js
@@ -1,11 +1,16 @@
 
 /**
+ * Adds two values.
+ *
  * @param {Number} a
  * @param {Number} b
  * @return {Number} The result of the add of the two parameters
  * @example
  *
  *  add(2, 3)  // 5
+ *  add('2', 3)  // 5
+ *  add('2', '3')  // 5
+ *  add('aa', 3)  // NaN
  */
 
 export default add = (a, b) => Number(a) + Number(b)
