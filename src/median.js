92ad260e9f34f5801a3dabc0ff08f2b70471ab36
builtbyedgar <hello@builtbyedgar.com>
2017-06-23 17:16:29 +0200

    added median

---
+++ src/median.js
@@ -0,0 +1,26 @@
+
+import mean from './mean'
+
+/**
+ *  Returns the median of the given list of numbers.
+ *
+ * @param {Array} list
+ * @return {Number}
+ * @example
+ *
+ *  median([2, 9, 7])  // 7
+ *  median([7, 2, 10, 9])  // 8
+ *  median([1, 1, 2, 2])  // 1.5
+ *  median([])  // NaN
+ */
+
+export default median = list => {
+  const { length } = list
+  if (length === 0) return NaN
+
+  const w = 2 - length % 2
+  const i = (length - w) / 2
+  return mean(Array.prototype.slice.call(list, 0)
+                                   .sort((a, b) => a < b ? -1 : a > b ? 1 : 0)
+                                   .slice(i, i + w))
+}
